f9b186780e52ce47f46960d57ad6d1538177d9d5
samkahchiin <samkahchiin@gmail.com>
2020-05-11 15:55:40 +0800

    WIP - Added payslips controller

--- app/controllers/payslips_controller.rb
+++ app/controllers/payslips_controller.rb
@@ -1,4 +1,13 @@
 class PayslipsController < ApplicationController
   def generate_monthly_payslip
+    if params[:name].nil? || params[:gross_annual_income].nil?
+      render json: { error_messages: 'Insufficient Info' }, status: :bad_request
+    else
+      res = TaxCalculator.generate_monthly_payslip(
+        params[:name],
+        params[:gross_annual_income]
+      )
+      render json: res.as_json, status: :ok
+    end
   end
 end
