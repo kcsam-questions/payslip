fe4e1230f241b403fa935c58d66b3450fd5a79d7
samkahchiin <samkahchiin@gmail.com>
2020-05-11 16:05:19 +0800

    [Finishes] Added payslips controller and spec
    - Added generate_monthly_payslip method
    - https://www.pivotaltracker.com/story/show/172758290

---
+++ spec/controller/payslips_controller_spec.rb
@@ -0,0 +1,48 @@
+require_relative '../rails_helper'
+require 'byebug'
+
+RSpec.describe PayslipsController, type: :controller do
+  describe 'GET #generate_monthly_payslip' do
+    before do
+      get :generate_monthly_payslip, params: params
+    end
+
+    context 'when params are provided' do
+      let(:name)   { 'Alice' }
+      let(:params) do
+        {
+          name: name,
+          gross_annual_income: 60_000
+        }
+      end
+      let(:expected_response) do
+        {
+          'employee_name': name,
+          'gross_monthly_income': '$5000.00',
+          'monthly_income_tax': '$500.00',
+          'net_monthly_income': '$4500.00'
+        }.as_json
+      end
+
+      it 'returns 200' do
+        expect(response.response_code).to eq 200
+      end
+
+      it 'returns the monthly payslips' do
+        expect(JSON.parse(response.body)).to eq expected_response
+      end
+    end
+
+    context 'when params are not provided' do
+      let(:params) { nil }
+
+      it 'returns 400' do
+        expect(response.response_code).to eq 400
+      end
+
+      it 'returns the monthly payslips' do
+        expect(JSON.parse(response.body)['error_messages']).to eq 'Insufficient Info'
+      end
+    end
+  end
+end
